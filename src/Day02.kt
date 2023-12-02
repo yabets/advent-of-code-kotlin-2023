a791ae94664c5b3b34cd4496c84f1d3969dfc655
Yabi <yabetsbelay@gmail.com>
2023-12-02 09:36:52 +0100

    Day02 part 1 solution

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,6 +1,26 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val map = mapOf("red" to 12, "green" to 13, "blue" to 14)
+        var result = 0
+        for(line in input) {
+            val game = line.substringBefore(":")
+            val id = game.substringAfter("Game ")
+            val isValid = line.substringAfter(":")
+                .split(";")
+                .stream()
+                .map { it.trim() }
+                .map { it.split(",") }
+                .flatMap { it.stream() }
+                .map { it.trim() }
+                .map{it.split(" ")}
+                .filter {it != null }
+                .filter{ it.size == 2 }
+                .filter{ map[it[1]]!! < it[0].toInt() }
+                .findAny()
+                .isEmpty
+            if(isValid) {result+= id.toInt()}
+        }
+        return result
     }
 
     fun part2(input: List<String>): Int {
@@ -8,10 +28,11 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day02_test")
+    part1(testInput).println()
+    check(part1(testInput) == 8)
 
-    val input = readInput("Day01")
+    val input = readInput("Day02")
     part1(input).println()
     part2(input).println()
 }
