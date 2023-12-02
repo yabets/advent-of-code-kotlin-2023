978c76d98fe2682610c6d89e5c761ad3928649df
Yabi <yabetsbelay@gmail.com>
2023-12-02 10:03:01 +0100

    Day02 part 2 solution

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,3 +1,5 @@
+import kotlin.math.max
+
 fun main() {
     fun part1(input: List<String>): Int {
         val map = mapOf("red" to 12, "green" to 13, "blue" to 14)
@@ -24,13 +26,29 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var result = 0
+        for(line in input) {
+            val map = mutableMapOf("red" to 0, "green" to 0, "blue" to 0)
+            line.substringAfter(":")
+                .split(";")
+                .stream()
+                .map { it.trim() }
+                .map { it.split(",") }
+                .flatMap { it.stream() }
+                .map { it.trim() }
+                .map{it.split(" ")}
+                .filter {it != null }
+                .filter{ it.size == 2 }
+                .forEach { map[it[1]] = max(map[it[1]]!!, it[0].toInt())}
+            result += map.values.filter { it != 0 }.reduce(Int::times)
+        }
+        return result
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
-    part1(testInput).println()
-    check(part1(testInput) == 8)
+    part2(testInput).println()
+    check(part2(testInput) == 2286)
 
     val input = readInput("Day02")
     part1(input).println()
