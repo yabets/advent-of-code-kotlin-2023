f541aa72f4f97ff82d177c57e0841bc88d8d9182
Yabi <yabetsbelay@gmail.com>
2023-12-03 13:59:08 +0100

    Day03 part 2 solution

--- src/Day03.kt
+++ src/Day03.kt
@@ -16,7 +16,7 @@ fun main() {
     fun part1(input: List<String>): Int {
         val board = input.stream().map { it.toList() }.toList()
         var result = 0
-        for(i in 0.rangeTo(board.size - 1)) {
+        for(i in 0..<board.size) {
             var j = 0
             while(j < board[i].size ) {
                 if(board[i][j].isDigit()) {
@@ -38,14 +38,58 @@ fun main() {
         return result
     }
 
+    fun getNumber(board: List<List<Char>>, x: Int, y: Int): Pair<Int,Pair<Int,Int>> {
+        var start = y
+        var end = y
+        // find start of number
+        while (start > 0 && board[x][start - 1].isDigit()) {
+            start--
+        }
+        // find end of number
+        while(end < board[x].size - 1 && board[x][end + 1].isDigit()) {
+            end++
+        }
+        val number = start.rangeTo(end).map { board[x][it] }.joinToString("").toInt()
+        return Pair(number, Pair(x, start))
+    }
+
+    fun findAdjacentNumbersPower(board: List<List<Char>>, i: Int, j: Int): Int {
+        val possibleNumbers = mutableSetOf<Pair<Int,Pair<Int, Int>>>()
+        for(x in max(i - 1, 0).rangeTo(min(i + 1, board.size - 1))) {
+            for(y in max(j - 1, 0).rangeTo(min(j + 1, board[i].size - 1))) {
+                if(board[x][y].isDigit()) {
+                    // get number
+                    val number = getNumber(board, x, y)
+                    possibleNumbers.add(number)
+                }
+            }
+        }
+        if(possibleNumbers.size != 2) {
+            return 0
+        }
+
+        return possibleNumbers.first().first * possibleNumbers.last().first
+    }
+
     fun part2(input: List<String>): Int {
-        return input.size
+        val board = input.stream().map { it.toList() }.toList()
+        var result = 0
+        for(i in 0..<board.size) {
+            var j = 0
+            while(j < board[i].size ) {
+                if(board[i][j] == '*') {
+                    result += findAdjacentNumbersPower(board, i, j)
+                }
+                j++
+            }
+        }
+
+        return result
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
-    part1(testInput).println()
-    check(part1(testInput) == 4361)
+    check(part2(testInput) == 467835)
 
     val input = readInput("Day03")
     part1(input).println()
